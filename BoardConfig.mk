e471401ffc03e8e9b7dc41507530035cd1a46bb5
xyyx <xyyx@mail.ru>
2017-12-16 22:06:13 +0800

    remove XiaomiParts

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -48,6 +48,10 @@ TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
 TARGET_KERNEL_SOURCE := kernel/xiaomi/vince
 TARGET_KERNEL_CONFIG := vince_defconfig
+TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_KERNEL_CLANG_VERSION := 7.0.2
+KBUILD_COMPILER_STRING := Android clang version 7.0.2
+export KBUILD_COMPILER_STRING
 
 # Enable real time lockscreen charging current values
 BOARD_GLOBAL_CFLAGS += -DBATTERY_REAL_INFO
